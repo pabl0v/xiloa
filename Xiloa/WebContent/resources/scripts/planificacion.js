041ed8da504f00b85980a37979bdc7cc5bf8537a
proyectoxiloa <dnchavez@hotmail.com>
2013-10-14 09:27:10 -0600

    CSS y xhtml

--- Xiloa/WebContent/resources/scripts/planificacion.js
+++ Xiloa/WebContent/resources/scripts/planificacion.js
@@ -1,3 +1,10 @@
 $(function(){
 	$("#boton_planificacion").addClass("selected");
+	
+	//Breadcrumbs para esta pagina:
+	$("#breadcrumbs").empty();
+	$("#breadcrumbs").append("<li>Inicio</li>");
+	$("#breadcrumbs").append("<li>Planificacion</li>");
+	
+	
 });
