3202bde214e3488ae54b2c7bed5d4eb051618705
proyectoxiloa <dnchavez@hotmail.com>
2013-10-15 15:37:19 -0600

    login modificado

--- Xiloa/WebContent/resources/scripts/usuario.js
+++ Xiloa/WebContent/resources/scripts/usuario.js
@@ -1,3 +1,18 @@
 $(function(){
 	$("#loginForm").appendTo("#aqui_formulario");
+	
+	var param1var = getQueryVariable("error");
 });
+
+
+function getQueryVariable(variable) {
+	  var query = window.location.search.substring(1);
+	  var vars = query.split("&");
+	  for (var i=0;i<vars.length;i++) {
+	    var pair = vars[i].split("=");
+	    if (pair[0] == variable) {
+	      return pair[1];
+	    }
+	  } 
+	  alert('Query Variable ' + variable + ' not found');
+}
