b31befae180cd2121a898ad8f8161ff80b0cad57
proyectoxiloa <dnchavez@hotmail.com>
2013-10-15 12:53:05 -0600

    Modificaciones en xhtml

--- Xiloa/WebContent/resources/scripts/paginamaestra.js
+++ Xiloa/WebContent/resources/scripts/paginamaestra.js
@@ -2,15 +2,19 @@ $(function(){
     $("#barra_navegacion").click(function(){
         if($("#barra_navegacion").hasClass("mini")){
             $(".botones_navegacion").fadeIn(100);
-            $("#barra_navegacion").animate({height: "75px"},100)
-            $("#barra_navegacion").removeClass("mini")
+            $("#barra_navegacion").animate({height: "75px"},100);
+            $("#barra_navegacion").removeClass("mini");
         }
         else {
-            $(".botones_navegacion").fadeOut(100,function(){
-                $("#barra_navegacion").animate({height: "25px"},100)
-                $("#barra_navegacion").addClass("mini")
-            });
+          
         }      
-    })
+    });
     $("#barra_navegacion").click();
+    $("#barra_navegacion").mouseleave(function(){
+    	$(".botones_navegacion").fadeOut(100,function(){
+            $("#barra_navegacion").animate({height: "25px"},100);
+            $("#barra_navegacion").addClass("mini");
+        });	
+    });
+    
 })
