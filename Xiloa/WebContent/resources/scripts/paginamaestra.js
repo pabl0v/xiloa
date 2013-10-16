afcdc95b8d6d4298fef50a091526a8f3b1f49ddc
proyectoxiloa <dnchavez@hotmail.com>
2013-10-16 12:00:22 -0600

    Mejoras en la barra superior de navegacion

--- Xiloa/WebContent/resources/scripts/paginamaestra.js
+++ Xiloa/WebContent/resources/scripts/paginamaestra.js
@@ -1,7 +1,7 @@
 $(function(){
     $("#barra_navegacion").click(function(){
         if($("#barra_navegacion").hasClass("mini")){
-            $(".botones_navegacion").fadeIn(100);
+            $(".botones_navegacion").css("display","inline-block");
             $("#barra_navegacion").animate({height: "75px"},100);
             $("#barra_navegacion").removeClass("mini");
         }
@@ -10,7 +10,7 @@ $(function(){
         }      
         
     });
-    $("#barra_navegacion").click();
+    
     $("#barra_navegacion").mouseleave(function(){
     	$(".botones_navegacion").fadeOut(100,function(){
             $("#barra_navegacion").animate({height: "25px"},100);
