cdacaf543eac60fe9c067ecb9dca6eaf3b02f2e4
proyectoxiloa <dnchavez@hotmail.com>
2013-10-17 17:13:48 -0600

    CSS y JS

--- Xiloa/WebContent/resources/scripts/edicion_planificacion.js
+++ Xiloa/WebContent/resources/scripts/edicion_planificacion.js
@@ -7,12 +7,4 @@ $(function(){
 	$("#breadcrumbs").append("<li>Planificacion</li>");
 	$("#breadcrumbs").append("<li>Editando planificacion</li>");
 	
-	/*$("#contenedor_tabla_unidadcomp").find("table").find("tbody").children("tr").click(function(){
-		
-		var idCentro = $(this).children("td").eq(0).text();
-		var idUCompetencia = $(this).children("td").eq(1).text();
-		
-		alert(idCentro + "," + idUCompetencia);
-		window.location = "/modulos/planificacion/edicion_planificacion.xhtml";
-	});*/
 });
--- Xiloa/WebContent/resources/scripts/planificacion.js
+++ Xiloa/WebContent/resources/scripts/planificacion.js
@@ -6,12 +6,4 @@ $(function(){
 	$("#breadcrumbs").append("<li>Inicio</li>");
 	$("#breadcrumbs").append("<li>Planificacion</li>");
 	
-	/*$("#contenedor_tabla_unidadcomp").find("table").find("tbody").children("tr").click(function(){
-		
-		var idCentro = $(this).children("td").eq(0).text();
-		var idUCompetencia = $(this).children("td").eq(1).text();
-		
-		alert(idCentro + "," + idUCompetencia);
-		window.location = "/modulos/planificacion/edicion_planificacion.xhtml";
-	});*/
 });
