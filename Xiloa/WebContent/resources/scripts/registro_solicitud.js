8d0a8e900bdfdb0c177b117db6e2f487db2fbe1e
proyectoxiloa <dnchavez@hotmail.com>
2013-10-17 16:15:14 -0600

    Mejoras en las interfaces

---
+++ Xiloa/WebContent/resources/scripts/registro_solicitud.js
@@ -0,0 +1,10 @@
+$(function(){
+	$("#boton_solicitudes").addClass("selected");
+	
+	//Breadcrumbs para esta pagina:
+	$("#breadcrumbs").empty();
+	$("#breadcrumbs").append("<li>Inicio</li>");
+	$("#breadcrumbs").append("<li>Solicitudes</li>");
+	$("#breadcrumbs").append("<li>Editando solicitud</li>");
+	
+});
