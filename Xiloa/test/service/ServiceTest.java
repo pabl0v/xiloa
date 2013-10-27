a35e7079720cfd3713da1307165db68ab3c40a31
Usuario-PC <Usuario-PC@Usuario.lan>
2013-10-27 14:40:09 -0600

    Insercion de datos de prueba a traves de JUnit para no alterar la secuencia de las tablas

--- Xiloa/test/service/ServiceTest.java
+++ Xiloa/test/service/ServiceTest.java
@@ -33,6 +33,10 @@ public class ServiceTest {
 
 	@BeforeClass
 	public static void setUp() throws Exception {
+	}
+
+	@Test
+	public void registrarMantenedores(){
 		
 		//creando mantenedores
 		
@@ -47,39 +51,7 @@ public class ServiceTest {
 		mantenedor7 = new Mantenedor("3","Estatus certificaciones","Pendiente",null,null);
 		mantenedor8 = new Mantenedor("3","Estatus certificaciones","Activo",null,null);
 		mantenedor9 = new Mantenedor("3","Estatus certificaciones","Anulado",null,null);
-		
-		//creando perfiles
-				
-		perfil = new Perfil("admin","admin","all","all","all",true,true);
-		perfiles = new ArrayList<Perfil>();
-		
-		//creando roles
-						
-		rolAdmin = new Rol("admin","admin",null,perfiles,true);
-		rolSupervisor = new Rol("supervisor","supervisor",213,perfiles,true);
-		rolVerificador = new Rol("verificador","Verificador",214,perfiles,true);
-		rolDocente = new Rol("tecnico_docente","Tecnico Docente",215,perfiles,true);
-		rolRegistrador = new Rol("registro_academico","Registro Academico",216,perfiles,true);
-		rolVisitante = new Rol("visitante","Visitante",null,perfiles,true);
-		
-		//creando usuarios
-				
-		usuarioAdmin = new Usuario(null,"admin","admin",rolAdmin,true);		
-		usuarioLopez = new Usuario(null,"nlopez","nlopez",rolAdmin,true);
-		usuarioRuiz = new Usuario(null,"aruiz","aruiz",rolAdmin,true);
-		usuarioPerez = new Usuario(null,"lperez","lperez",rolAdmin,true);
-		usuarioArauz = new Usuario(null,"larauz","larauz",rolAdmin,true);
-		
-		//creando contactos
-		
-		contactoLopez = new Contacto(null,null,1,"Norcecy",null,"Lopez",null,"Norcecy Lopez",1,"nlopez@inatec.edu.ni",null,"00000000",null,1,1,"12345678901234","Managua",new Date(),new Date(),1,"Managua",false,null,null,null);
-		contactoRuiz = new Contacto(null,null,1,"Ana",null,"Ruiz",null,"Ana Ruiz",1,"aruiz@inatec.edu.ni",null,"00000000",null,1,1,"12345678901234","Managua",new Date(),new Date(),1,"Managua",false,null,null,null);
-		contactoPerez = new Contacto(null,null,1,"Luis",null,"Perez",null,"Luis Perez",1,"lperez@inatec.edu.ni",null,"00000000",null,1,1,"12345678901234","Managua",new Date(),new Date(),1,"Managua",false,null,null,null);
-		contactoArauz = new Contacto(null,null,1,"Luz",null,"Arauz",null,"Luz Arauz",1,"larauz@inatec.edu.ni",null,"00000000",null,1,1,"12345678901234","Managua",new Date(),new Date(),1,"Managua",false,null,null,null);
-	}
 
-	@Test
-	public void registrarMantenedores(){
 		service.guardar(mantenedor1);
 		service.guardar(mantenedor2);
 		service.guardar(mantenedor3);
@@ -89,8 +61,22 @@ public class ServiceTest {
 		service.guardar(mantenedor7);
 		service.guardar(mantenedor8);
 		service.guardar(mantenedor9);
+
+		//creando perfiles
+		
+		perfil = new Perfil("admin","admin","all","all","all",true,true);
+		
+		//creando roles
+						
+		rolAdmin = new Rol("admin","admin",null,null,true);
+		rolSupervisor = new Rol("supervisor","supervisor",213,null,true);
+		rolVerificador = new Rol("verificador","Verificador",214,null,true);
+		rolDocente = new Rol("tecnico_docente","Tecnico Docente",215,null,true);
+		rolRegistrador = new Rol("registro_academico","Registro Academico",216,null,true);
+		rolVisitante = new Rol("visitante","Visitante",null,null,true);
 		
 		perfil = (Perfil)service.guardar(perfil);
+		perfiles = new ArrayList<Perfil>();
 		perfiles.add(perfil);
 		
 		rolAdmin.setPerfiles(perfiles);
@@ -100,77 +86,103 @@ public class ServiceTest {
 		rolRegistrador.setPerfiles(perfiles);
 		rolVisitante.setPerfiles(perfiles);
 		
-		service.guardar(rolAdmin);
-		service.guardar(rolSupervisor);
-		service.guardar(rolVerificador);
-		service.guardar(rolDocente);
-		service.guardar(rolRegistrador);
-		service.guardar(rolVisitante);
+		rolAdmin = (Rol)service.guardar(rolAdmin);
+		rolSupervisor = (Rol)service.guardar(rolSupervisor);
+		rolVerificador = (Rol)service.guardar(rolVerificador);
+		rolDocente = (Rol)service.guardar(rolDocente);
+		rolRegistrador = (Rol)service.guardar(rolRegistrador);
+		rolVisitante = (Rol)service.guardar(rolVisitante);
+		
+		//creando usuarios
+		
+		usuarioAdmin = new Usuario(null,"admin","admin",rolAdmin,true);		
+		usuarioLopez = new Usuario(null,"nlopez","nlopez",rolAdmin,true);
+		usuarioRuiz = new Usuario(null,"aruiz","aruiz",rolAdmin,true);
+		usuarioPerez = new Usuario(null,"lperez","lperez",rolAdmin,true);
+		usuarioArauz = new Usuario(null,"larauz","larauz",rolAdmin,true);
 		
 		usuarioAdmin = (Usuario)service.guardar(usuarioAdmin);
 		usuarioArauz = (Usuario)service.guardar(usuarioArauz);
 		usuarioLopez = (Usuario)service.guardar(usuarioLopez);
 		usuarioPerez = (Usuario)service.guardar(usuarioPerez);
-		usuarioRuiz = (Usuario)service.guardar(usuarioRuiz);
+		usuarioRuiz = (Usuario)service.guardar(usuarioRuiz);		
 		
-		contactoLopez = (Contacto)service.guardar(contactoLopez);
-		contactoRuiz = (Contacto)service.guardar(contactoRuiz);
-		contactoPerez = (Contacto)service.guardar(contactoPerez);
-		contactoArauz = (Contacto)service.guardar(contactoArauz);
+		//creando contactos
 		
-		contactoLopez.setUsuario(usuarioLopez);
-		contactoRuiz.setUsuario(usuarioRuiz);
-		contactoPerez.setUsuario(usuarioPerez);
-		contactoArauz.setUsuario(usuarioArauz);
-		
-		contactoLopez = (Contacto)service.guardar(contactoLopez);
-		contactoRuiz = (Contacto)service.guardar(contactoRuiz);
-		contactoPerez = (Contacto)service.guardar(contactoPerez);
-		contactoArauz = (Contacto)service.guardar(contactoArauz);
-	}
-/*	
-	@Test
-	public void registrarPerfiles(){
-		
-		perfil = (Perfil)service.guardar(perfil);
-		perfiles.add(perfil);
-		
-		rolAdmin.setPerfiles(perfiles);
-		rolSupervisor.setPerfiles(perfiles);
-		rolVerificador.setPerfiles(perfiles);
-		rolDocente.setPerfiles(perfiles);
-		rolRegistrador.setPerfiles(perfiles);
-		rolVisitante.setPerfiles(perfiles);
-		
-		service.guardar(rolAdmin);
-		service.guardar(rolSupervisor);
-		service.guardar(rolVerificador);
-		service.guardar(rolDocente);
-		service.guardar(rolRegistrador);
-		service.guardar(rolVisitante);
-	}
-	
-	public void registrarUsuarios(){
-		
-		usuarioAdmin = (Usuario)service.guardar(usuarioAdmin);
-		usuarioArauz = (Usuario)service.guardar(usuarioArauz);
-		usuarioLopez = (Usuario)service.guardar(usuarioLopez);
-		usuarioPerez = (Usuario)service.guardar(usuarioPerez);
-		usuarioRuiz = (Usuario)service.guardar(usuarioRuiz);
-		
-		contactoLopez = (Contacto)service.guardar(contactoLopez);
-		contactoRuiz = (Contacto)service.guardar(contactoRuiz);
-		contactoPerez = (Contacto)service.guardar(contactoPerez);
-		contactoArauz = (Contacto)service.guardar(contactoArauz);
-		
-		contactoLopez.setUsuario(usuarioLopez);
-		contactoRuiz.setUsuario(usuarioRuiz);
-		contactoPerez.setUsuario(usuarioPerez);
-		contactoArauz.setUsuario(usuarioArauz);
-		
+		contactoLopez = new Contacto(usuarioLopez,null,1,"Norcecy",null,"Lopez",null,"Norcecy Lopez",1,"nlopez@inatec.edu.ni",null,"00000000",null,1,1,"12345678901234","Managua",new Date(),new Date(),1,"Managua",false,null,null,null);
+		contactoRuiz = new Contacto(usuarioRuiz,null,1,"Ana",null,"Ruiz",null,"Ana Ruiz",1,"aruiz@inatec.edu.ni",null,"00000000",null,1,1,"12345678901234","Managua",new Date(),new Date(),1,"Managua",false,null,null,null);
+		contactoPerez = new Contacto(usuarioPerez,null,1,"Luis",null,"Perez",null,"Luis Perez",1,"lperez@inatec.edu.ni",null,"00000000",null,1,1,"12345678901234","Managua",new Date(),new Date(),1,"Managua",false,null,null,null);
+		contactoArauz = new Contacto(usuarioArauz,null,1,"Luz",null,"Arauz",null,"Luz Arauz",1,"larauz@inatec.edu.ni",null,"00000000",null,1,1,"12345678901234","Managua",new Date(),new Date(),1,"Managua",false,null,null,null);
+
 		contactoLopez = (Contacto)service.guardar(contactoLopez);
 		contactoRuiz = (Contacto)service.guardar(contactoRuiz);
 		contactoPerez = (Contacto)service.guardar(contactoPerez);
 		contactoArauz = (Contacto)service.guardar(contactoArauz);		
+	}
+
+	/*
+	@Test
+	public void registrarPerfiles(){
+		
+		//creando perfiles
+		
+		perfil = new Perfil("admin","admin","all","all","all",true,true);
+		
+		//creando roles
+						
+		rolAdmin = new Rol("admin","admin",null,null,true);
+		rolSupervisor = new Rol("supervisor","supervisor",213,null,true);
+		rolVerificador = new Rol("verificador","Verificador",214,null,true);
+		rolDocente = new Rol("tecnico_docente","Tecnico Docente",215,null,true);
+		rolRegistrador = new Rol("registro_academico","Registro Academico",216,null,true);
+		rolVisitante = new Rol("visitante","Visitante",null,null,true);
+		
+		perfil = (Perfil)service.guardar(perfil);
+		perfiles = new ArrayList<Perfil>();
+		perfiles.add(perfil);
+		
+		rolAdmin.setPerfiles(perfiles);
+		rolSupervisor.setPerfiles(perfiles);
+		rolVerificador.setPerfiles(perfiles);
+		rolDocente.setPerfiles(perfiles);
+		rolRegistrador.setPerfiles(perfiles);
+		rolVisitante.setPerfiles(perfiles);
+		
+		rolAdmin = (Rol)service.guardar(rolAdmin);
+		rolSupervisor = (Rol)service.guardar(rolSupervisor);
+		rolVerificador = (Rol)service.guardar(rolVerificador);
+		rolDocente = (Rol)service.guardar(rolDocente);
+		rolRegistrador = (Rol)service.guardar(rolRegistrador);
+		rolVisitante = (Rol)service.guardar(rolVisitante);
+	}
+	
+	@Test
+	public void registrarUsuarios(){
+		
+		//creando usuarios
+		
+		usuarioAdmin = new Usuario(null,"admin","admin",rolAdmin,true);		
+		usuarioLopez = new Usuario(null,"nlopez","nlopez",rolAdmin,true);
+		usuarioRuiz = new Usuario(null,"aruiz","aruiz",rolAdmin,true);
+		usuarioPerez = new Usuario(null,"lperez","lperez",rolAdmin,true);
+		usuarioArauz = new Usuario(null,"larauz","larauz",rolAdmin,true);
+		
+		usuarioAdmin = (Usuario)service.guardar(usuarioAdmin);
+		usuarioArauz = (Usuario)service.guardar(usuarioArauz);
+		usuarioLopez = (Usuario)service.guardar(usuarioLopez);
+		usuarioPerez = (Usuario)service.guardar(usuarioPerez);
+		usuarioRuiz = (Usuario)service.guardar(usuarioRuiz);		
+		
+		//creando contactos
+		
+		contactoLopez = new Contacto(usuarioLopez,null,1,"Norcecy",null,"Lopez",null,"Norcecy Lopez",1,"nlopez@inatec.edu.ni",null,"00000000",null,1,1,"12345678901234","Managua",new Date(),new Date(),1,"Managua",false,null,null,null);
+		contactoRuiz = new Contacto(usuarioRuiz,null,1,"Ana",null,"Ruiz",null,"Ana Ruiz",1,"aruiz@inatec.edu.ni",null,"00000000",null,1,1,"12345678901234","Managua",new Date(),new Date(),1,"Managua",false,null,null,null);
+		contactoPerez = new Contacto(usuarioPerez,null,1,"Luis",null,"Perez",null,"Luis Perez",1,"lperez@inatec.edu.ni",null,"00000000",null,1,1,"12345678901234","Managua",new Date(),new Date(),1,"Managua",false,null,null,null);
+		contactoArauz = new Contacto(usuarioArauz,null,1,"Luz",null,"Arauz",null,"Luz Arauz",1,"larauz@inatec.edu.ni",null,"00000000",null,1,1,"12345678901234","Managua",new Date(),new Date(),1,"Managua",false,null,null,null);
+
+		contactoLopez = (Contacto)service.guardar(contactoLopez);
+		contactoRuiz = (Contacto)service.guardar(contactoRuiz);
+		contactoPerez = (Contacto)service.guardar(contactoPerez);
+		contactoArauz = (Contacto)service.guardar(contactoArauz);				
 	}*/
 }
