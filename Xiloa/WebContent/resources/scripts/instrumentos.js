051b4f04fae4452dd39a8decb3c19cf8a9614c45
Jose Malespin <Jose Malespin@192.168.2.47>
2013-11-05 16:21:25 -0600

    Cambios en edicion planificacion y agregada vista para instrumentos

---
+++ Xiloa/WebContent/resources/scripts/instrumentos.js
@@ -0,0 +1,11 @@
+$(function(){
+	$("#boton_planificacion").addClass("selected");
+	
+	//Breadcrumbs para esta pagina:
+	$("#breadcrumbs").empty();
+	$("#breadcrumbs").append("<li>Inicio</li>");
+	$("#breadcrumbs").append("<li>Planificacion</li>");
+	$("#breadcrumbs").append("<li>Editando planificacion</li>");
+	$("#breadcrumbs").append("<li>Instrumentos a utilizar</li>");
+	
+});
