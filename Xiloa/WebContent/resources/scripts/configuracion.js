b52bd0097fa7efe24e3d6d53606b0f9440879650
Usuario <Usuario@chontales.inatec.edu.ni>
2013-11-22 14:58:06 -0600

    Modificacion de estilos

---
+++ Xiloa/WebContent/resources/scripts/configuracion.js
@@ -0,0 +1,8 @@
+$(function(){
+	$("#boton_solicitudes").addClass("selected");
+	
+	//Breadcrumbs para esta pagina:
+	$("#breadcrumbs").empty();
+	$("#breadcrumbs").append("<li>Inicio</li>");
+	$("#breadcrumbs").append("<li>Configuracion</li>");
+});
