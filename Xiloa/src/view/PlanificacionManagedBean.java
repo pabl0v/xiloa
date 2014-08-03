6179192431dae76ec1cec63356fe5e36aca10532
Usuario-PC <Usuario-PC@amatlone.lan>
2014-08-02 23:09:25 -0600

    Reorganizando la planificacion: edicion de certificacion y actividades

--- Xiloa/src/view/PlanificacionManagedBean.java
+++ Xiloa/src/view/PlanificacionManagedBean.java
@@ -3,22 +3,28 @@ package view;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.faces.model.SelectItem;
 
+import model.Actividad;
 import model.Certificacion;
+import model.Contacto;
+import model.Involucrado;
 import model.Mantenedor;
 
 import org.primefaces.event.SelectEvent;
+import org.primefaces.event.TransferEvent;
+import org.primefaces.model.DualListModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import controller.LoginController;
 import service.IService;
-import support.FacesUtil;
 import support.Ifp;
 import support.UCompetencia;
 
@@ -44,31 +50,57 @@ public class PlanificacionManagedBean implements Serializable {
 	private List<Certificacion> certificaciones;
 	private UCompetencia selectedCompetencia;
 	private Certificacion selectedCertificacion;
-	
+	private Map<Integer, Mantenedor> catalogoEstatusCertificacion;
+	private Integer selectedEstatusCertificacion;
+	private List<Actividad> actividades;
+	private Actividad selectedActividad;
+	private Map<Integer, Mantenedor> catalogoEstatusActividad;
+	private Integer selectedEstatusActividad;
+	private DualListModel<Contacto> involucrados;
+
 	public PlanificacionManagedBean(){
 		super();
 		certificaciones = new ArrayList<Certificacion>();
+		catalogoEstatusCertificacion = new HashMap<Integer, Mantenedor>();
+		catalogoEstatusActividad = new HashMap<Integer, Mantenedor>();
+		actividades = new ArrayList<Actividad>();
+		involucrados = new DualListModel<Contacto>();
 	}
 	
 	@PostConstruct
 	private void init(){
 		certificaciones = service.getCertificaciones(controller.getEntidadUsuario());
+		catalogoEstatusCertificacion = service.getMapMantenedoresByTipo("3");
+		catalogoEstatusActividad = service.getMapMantenedoresByTipo("2");
+		setInvolucrados(new DualListModel<Contacto>(new ArrayList<Contacto>(), new ArrayList<Contacto>()));
 	}
 	
 	public List<Certificacion> getCertificaciones(){
 		return certificaciones;
 	}
-		
-	public List<UCompetencia> getCompetencias(){
-		return service.getUcompetenciaSinPlanificar(controller.getEntidadUsuario());
-	}
-		
+	
 	public Certificacion getSelectedCertificacion() {
 		return selectedCertificacion;
 	}
 
 	public void setSelectedCertificacion(Certificacion selectedCertificacion) {
 		this.selectedCertificacion = selectedCertificacion;
+	}
+	
+	public Integer getSelectedEstatusCertificacion(){
+		return selectedEstatusCertificacion;
+	}
+	
+	public void setSelectedEstatusCertificacion(Integer estatus){
+		this.selectedEstatusCertificacion = estatus;
+	}
+		
+	public List<Mantenedor> getCatalogoEstatusCertificacion() {
+		return new ArrayList<Mantenedor>(this.catalogoEstatusCertificacion.values());
+	}
+
+	public List<UCompetencia> getCompetencias(){
+		return service.getUcompetenciaSinPlanificar(controller.getEntidadUsuario());
 	}
 
 	public UCompetencia getSelectedCompetencia() {
@@ -77,6 +109,93 @@ public class PlanificacionManagedBean implements Serializable {
 	
 	public void setSelectedCompetencia(UCompetencia selectedCompetencia) {
 		this.selectedCompetencia = selectedCompetencia;
+	}
+	
+	public List<Mantenedor> getCatalogoEstatusActividad() {
+		return new ArrayList<Mantenedor>(this.catalogoEstatusActividad.values());
+	}
+	
+	public Integer getSelectedEstatusActividad() {
+		return selectedEstatusActividad;
+	}
+	
+	public void setSelectedEstatusActividad(Integer estatus){
+		this.selectedEstatusActividad = estatus;
+	}
+	
+	public List<Actividad> getActividades(){
+		return actividades;
+	}
+	
+	public void setActividades(List<Actividad> actividades){
+		this.actividades = actividades;
+	}
+	
+	public Actividad getSelectedActividad(){
+		return selectedActividad;
+	}
+	
+	public void setSelectedActividad(Actividad actividad){
+		this.selectedActividad = actividad;
+		if(selectedActividad.getId() == null)
+			setSelectedEstatusActividad(null);
+		else
+			setSelectedEstatusActividad(selectedActividad.getEstado().getId());
+	}
+	
+	public DualListModel<Contacto> getInvolucrados(){
+		return involucrados;
+	}
+	
+	public void setInvolucrados(DualListModel<Contacto> involucrados){
+		this.involucrados = involucrados;
+	}
+	
+	public void onActividadSelect(SelectEvent event){
+		setSelectedActividad((Actividad) event.getObject());
+		setSelectedEstatusActividad(selectedActividad.getEstado().getId());
+		setInvolucrados(new DualListModel<Contacto>(service.getInvolucradosNotInActividadId(selectedActividad.getId()), service.getInvolucradosInActividadId(selectedActividad.getId())));		
+	}
+	
+	public void onActividadUnselect(SelectEvent event){		
+	}
+	
+	public void onCertificacionSelect(SelectEvent event){
+		setSelectedCertificacion((Certificacion) event.getObject());
+		setSelectedEstatusCertificacion(selectedCertificacion.getEstatus().getId());
+		setActividades(service.getActividades(selectedCertificacion.getId()));
+		setSelectedActividad(new Actividad());
+	}
+	
+	public void onCertificacionUnselect(SelectEvent event){		
+	}
+	
+	public void onElementTransfer(TransferEvent event){
+		
+		// agregar involucrado
+		if(event.isAdd()){
+			for(Object item : event.getItems()) {
+				if(item instanceof Contacto){
+					Contacto contacto = (Contacto) item;
+					service.guardar(new Involucrado(selectedActividad, contacto));
+				}
+			}
+		}
+		
+		//remover involucrado
+		if(event.isRemove()){
+			for(Object item : event.getItems()) {
+				if(item instanceof Contacto){
+					Contacto contacto = (Contacto) item;
+					service.inactivarInvolucrado(selectedActividad.getId(), contacto.getId());
+				}
+			}
+		}
+	}
+	
+	public void actualizarActividad(Actividad actividad){
+		actividad.setEstado(catalogoEstatusActividad.get(selectedEstatusActividad));
+		service.guardar(actividad);
 	}
 	
 	public void nuevaCertificacion(UCompetencia competencia){
@@ -107,10 +226,17 @@ public class PlanificacionManagedBean implements Serializable {
 		certificaciones = service.getCertificaciones(controller.getEntidadUsuario());
 	}
 	
-	public String editarCertificacion(Certificacion certificacion){
+	public void actualizarCertificacion(Certificacion certificacion){
+		certificacion.setEstatus(catalogoEstatusCertificacion.get(selectedEstatusCertificacion));
+		service.guardar(certificacion);
+	}
+	
+	/*
+	public String editarActividades(Certificacion certificacion){
 		FacesUtil.setParamBySession("certificacionId", certificacion.getId());
 		return "/modulos/planificacion/edicion_planificacion?faces-redirect=true";
 	}
+	*/
 	
 	public void onRowSelectCompetencia(SelectEvent event) {  
     }
